61a039b53157cda048426ed9fbd4ec315a1c44b1
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-04-16 11:43:33 -0600

    SQL Tables

--- server/Setup.sql
+++ server/Setup.sql
@@ -1,6 +1,41 @@
+-- NOTE: Profiles
 -- CREATE TABLE profiles (
 --    id VARCHAR(255) NOT NULL PRIMARY KEY,
 --    name VARCHAR(255),
 --    email VARCHAR(255) NOT NULL UNIQUE,
 --    picture VARCHAR(255)
+-- );
+-- NOTE: Vaults
+-- CREATE TABLE vaults (
+--    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--    creatorId VARCHAR(255) NOT NULL,
+--    name VARCHAR(255) NOT NULL,
+--    description VARCHAR(255) NOT NULL,
+--    isPrivate TINYINT,
+--    FOREIGN KEY (creatorId) REFERENCES profiles (id)
+-- );
+-- NOTE: Keeps
+-- CREATE TABLE keeps (
+--    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--    creatorId VARCHAR(255) NOT NULL,
+--    name VARCHAR(255) NOT NULL,
+--    description VARCHAR(255) NOT NULL,
+--    image VARCHAR(255) NOT NULL,
+--    views INT,
+--    shares INT,
+--    keeps INT,
+--    FOREIGN KEY (creatorId) REFERENCES profiles (id)
+-- );
+-- NOTE: VaultKeeps
+-- CREATE TABLE vaultKeeps (
+--    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--    creatorId VARCHAR(255) NOT NULL,
+--    vaultId INT NOT NULL,
+--    keepId INT NOT NULL,
+--    FOREIGN KEY (creatorId)
+--       REFERENCES profiles (id),
+--    FOREIGN KEY (vaultId)
+--       REFERENCES vaults (id),
+--    FOREIGN KEY (keepId)
+--       REFERENCES keeps (id)
 -- );
