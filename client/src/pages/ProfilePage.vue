b8ff1f7ed19b065514d0bb82179f8619b533f9f8
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-04-20 15:15:16 -0600

    Profile page and services

--- client/src/pages/ProfilePage.vue
+++ client/src/pages/ProfilePage.vue
@@ -1,20 +1,58 @@
 <template>
-  <div class="ProfilePage">
-    <img class="rounded" :src="state.activeProfile.picture" :alt="state.activeProfile.email" />
-    <p>{{ state.activeProfile.email }}</p>
+  <div class="ProfilePage container">
+    <div class="row">
+      <div class="col">
+        <div class="text-center p-1">
+          <img class="rounded" :src="state.activeProfile.picture" :alt="state.activeProfile.email" />
+          <p>{{ state.activeProfile.email }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h4>Vaults</h4>
+      </div>
+      <div class="col-3" v-for="vault in state.vaults" :key="vault.id">
+        <div class="card p-2 my-1 shadow">
+          <p>{{ vault.name }}</p>
+          <p>{{ vault.description }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h4>Keeps</h4>
+      </div>
+      <div class="col-3" v-for="keep in state.keeps" :key="keep.id">
+        <div class="card shadow my-1">
+          <p>{{ keep.name }}</p>
+          <p>{{ keep.description }}</p>
+          <img class="img-fluid" :src="keep.image">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed, reactive } from 'vue'
+import { computed, onMounted, reactive } from 'vue'
 import { AppState } from '../AppState.js'
-// import { useRoute } from 'vue-router'
+import { useRoute } from 'vue-router'
+import { profileService } from '../services/ProfileService'
 export default {
   name: 'ProfilePage',
   setup() {
-    // const route = useRoute()
+    const route = useRoute()
     const state = reactive({
-      activeProfile: computed(() => AppState.activeProfile)
+      activeProfile: computed(() => AppState.activeProfile),
+      keeps: computed(() => AppState.keeps),
+      vaults: computed(() => AppState.vaults)
+    })
+    onMounted(() => {
+      const id = route.params.profileId
+      profileService.GetProfileById(id)
+      profileService.GetUsersKeeps(id)
+      profileService.GetUsersVaults(id)
     })
     return {
       state
--- client/src/services/ProfileService.js
+++ client/src/services/ProfileService.js
@@ -1,9 +1,15 @@
+import { AppState } from '../AppState.js'
+import { Keep } from '../models/KeepModel.js'
+import { Vault } from '../models/VaultModel'
+import { Profile } from '../models/ProfileModel.js'
 import { logger } from '../utils/Logger.js'
+import { api } from './AxiosService.js'
 
 class ProfileService {
   async GetProfileById(id) {
     try {
-      // TODO
+      const res = await api.get('api/profiles/' + id)
+      AppState.activeProfile = new Profile(res.data)
     } catch (err) {
       logger.error(err)
     }
@@ -11,7 +17,8 @@ class ProfileService {
 
   async GetUsersKeeps(id) {
     try {
-      // TODO
+      const res = await api.get(`api/profiles/${id}/keeps`)
+      AppState.keeps = res.data.map(k => new Keep(k))
     } catch (err) {
       logger.error(err)
     }
@@ -19,7 +26,8 @@ class ProfileService {
 
   async GetUsersVaults(id) {
     try {
-      // TODO
+      const res = await api.get(`api/profiles/${id}/vaults`)
+      AppState.vaults = res.data.map(v => new Vault(v))
     } catch (err) {
       logger.error(err)
     }
