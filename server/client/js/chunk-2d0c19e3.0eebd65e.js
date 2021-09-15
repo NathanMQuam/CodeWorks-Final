77b520fe01807cca1376be05ed6580098c91dec1
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-09-15 17:03:47 -0600

    heroku build

---
+++ server/client/js/chunk-2d0c19e3.0eebd65e.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c19e3"],{"478b":function(e,t,n){"use strict";n.r(t);var a=n("7a23");const c={class:"about"},u=Object(a["g"])("h1",null,"This is the about page",-1),o=[u];function s(e,t,n,u,s,b){return Object(a["t"])(),Object(a["f"])("div",c,o)}var b={name:"AboutPage"};b.render=s;t["default"]=b}}]);
